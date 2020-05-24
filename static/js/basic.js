d4370d17f6c0c1991ea6ce74074325863538a8ae
Amiris <52610281+amirisbrown@users.noreply.github.com>
2020-05-24 16:07:23 -0500

    Add files via upload

--- static/js/basic.js
+++ static/js/basic.js
@@ -1,1 +1,18 @@
+$(document).ready(function() {
+	$('.menu-icon').on('click', function(e){
+		$('.menu').show();
+	});
+	$('.close-button').on('click', function(e){
+		$('.menu').hide();
+	});
+	currentSlide = 0;
+	$( '.control-prev' ).click(function() {
+		 ;
+		$( "" ).toggle( "slide" );
+	});
+	$( '.control-next' ).click(function() {
+		;
+		$( "" ).toggle( "slide" );
+	});
 
+})
