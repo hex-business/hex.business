143f6e8c82d12aa406157eb0110751ee46c986be
Amiris <52610281+amirisbrown@users.noreply.github.com>
2020-05-24 15:51:07 -0500

    Add files via upload

---
+++ hexbusiness-master/build/precache-manifest.35cab4535ed1537f07f64ae602a99426.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e958d2ea9546e2b8fcb52c709bf6ca6d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a83a0eba885d770821ff",
+    "url": "/static/css/2.7ea73bcd.chunk.css"
+  },
+  {
+    "revision": "bb2ba28f7b1c7d9265e5",
+    "url": "/static/css/main.a6c5cfa6.chunk.css"
+  },
+  {
+    "revision": "a83a0eba885d770821ff",
+    "url": "/static/js/2.3c124a4e.chunk.js"
+  },
+  {
+    "revision": "533e4a2892f8ebb1ce21c19867740c1e",
+    "url": "/static/js/2.3c124a4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb2ba28f7b1c7d9265e5",
+    "url": "/static/js/main.7126e1e3.chunk.js"
+  },
+  {
+    "revision": "e9f57e6f168b37a46325",
+    "url": "/static/js/runtime-main.738c7ee1.js"
+  },
+  {
+    "revision": "2e9fe4a0b372c3e237cde38751b56a46",
+    "url": "/static/media/1900x800HEXsplash-0001.2e9fe4a0.jpg"
+  },
+  {
+    "revision": "ceaea087c9a125c60a70e165dd7f5241",
+    "url": "/static/media/3HEXBanner1900x800.ceaea087.jpg"
+  },
+  {
+    "revision": "ee2067acc3e5135d31d18d65ba06fe0c",
+    "url": "/static/media/COMINGSOONgraphicFIN.ee2067ac.jpg"
+  },
+  {
+    "revision": "f7a7e4b97f40152ae3a7166d529d7319",
+    "url": "/static/media/HEXCredit.f7a7e4b9.png"
+  },
+  {
+    "revision": "8a7b779a7aaf3e454001009e760d6dda",
+    "url": "/static/media/HEXMoney.8a7b779a.png"
+  },
+  {
+    "revision": "502fd8bea3f3cc3bceafcc316f490771",
+    "url": "/static/media/HEXPlay.502fd8be.png"
+  },
+  {
+    "revision": "23bb336d6b12659b3c92f05201a1d1f2",
+    "url": "/static/media/HEXRocket.23bb336d.png"
+  },
+  {
+    "revision": "2affd5539d9c9a0e8da81669c1354653",
+    "url": "/static/media/HEXStable.2affd553.png"
+  },
+  {
+    "revision": "2602e5b328a03d3eb1390ce140201ff9",
+    "url": "/static/media/HEXStake.2602e5b3.png"
+  },
+  {
+    "revision": "0396ba585ff544d9f7ecf0ba91ccef95",
+    "url": "/static/media/HEXbet.0396ba58.png"
+  },
+  {
+    "revision": "8cbeea9c08b251312e31dd05dad1d232",
+    "url": "/static/media/HEXpool.8cbeea9c.png"
+  },
+  {
+    "revision": "d7a5a8488118d9ee4f35e783bdb8f1db",
+    "url": "/static/media/Hex-Mobile-update.d7a5a848.png"
+  },
+  {
+    "revision": "23129ffa9b70b0192c0c11ddc9f733f1",
+    "url": "/static/media/Hex-money-updated.23129ffa.png"
+  },
+  {
+    "revision": "63d09ae54f2aa13d0091618c75470a90",
+    "url": "/static/media/Palanquin-Medium.63d09ae5.ttf"
+  },
+  {
+    "revision": "d5bffc4873d0b2526f23072492da29e1",
+    "url": "/static/media/apk.d5bffc48.png"
+  },
+  {
+    "revision": "53d7f34fdafc9aab686d2711ee00e3a8",
+    "url": "/static/media/creditcards.53d7f34f.png"
+  },
+  {
+    "revision": "b90a792f020aa59bb8057f713c11dffc",
+    "url": "/static/media/googleplay.b90a792f.png"
+  },
+  {
+    "revision": "85e284d8eb230a42e0e97ccfdb82d9a0",
+    "url": "/static/media/hex-credit-update.85e284d8.png"
+  },
+  {
+    "revision": "7c611557596b6d667d82197e9f8551ae",
+    "url": "/static/media/hex-mobie-assetsl.7c611557.png"
+  },
+  {
+    "revision": "8f0141ec9743890066acaa4c59754a92",
+    "url": "/static/media/hexbusinessqrcode.8f0141ec.jpg"
+  },
+  {
+    "revision": "019a57c70f7ca228a1d597120517949d",
+    "url": "/static/media/hexbusinesstext.019a57c7.png"
+  },
+  {
+    "revision": "afdbd8e6c7badd2443b031570287b6fd",
+    "url": "/static/media/hexmoney promo41900x800 (1).afdbd8e6.jpg"
+  },
+  {
+    "revision": "4d7f4e71d0d4c455605e617ac7974871",
+    "url": "/static/media/ios.4d7f4e71.png"
+  },
+  {
+    "revision": "2c92daf35b56a3733fa3b4213c90c3ef",
+    "url": "/static/media/metamask.2c92daf3.png"
+  },
+  {
+    "revision": "fb8b964bae2da53a5633a5f300ce6330",
+    "url": "/static/media/mobile.fb8b964b.png"
+  }
+]);
