e8fdb8dc604a8b94370d834bbcd8efd6d2279f17
shaun <shaunnewsum2020@gmail.com>
2020-05-25 22:09:48 +0800

    update fetching part and api route

---
+++ server.php
@@ -0,0 +1,14 @@
+<?php
+	$totalSupply = $_POST["totalSupply"];
+	$maxSupply = $_POST["maxSupply"];
+	$tokenFrozenBalances = $_POST["tokenFrozenBalances"];
+	$balanceOf = $_POST["balanceOf"];
+	$freezingReward = $_POST["freezingReward"];
+	$lockedToken = $_POST["lockedToken"];
+	$frzoneTokenBalance = $_POST["frzoneTokenBalance"];
+	$allowance = $_POST["allowance"];
+	$metamaskEthBalance = $_POST["metamaskEthBalance"];
+
+	// Edwin Process
+
+?>
