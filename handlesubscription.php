1dec40a5ff8496fb65e682b02a2023bfe4973381
shaun <shaunnewsum2020@gmail.com>
2020-06-03 07:15:56 +0800

    update subscription

--- handlesubscription.php
+++ handlesubscription.php
@@ -19,10 +19,7 @@ if (empty($email))
 else
 {
 	$email = trim($email);
-
-	//$emailRegex = ">(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])>i";
-
-	//if (preg_match($emailRegex, $_POST['email']))
+	$email = filter_var($email, FILTER_SANITIZE_EMAIL);
 	
 	if (filter_var($email, FILTER_VALIDATE_EMAIL))
 	{
@@ -47,17 +44,17 @@ else
 }
 
 ?>
-<html>
-<head>
-	<link href="./static/css/additional.css" rel="stylesheet">
-	<link href="./static/css/main.css" rel="stylesheet">
-	<link rel="stylesheet" href="./static/css/basic.css?v=0.0.1">
 
-</head>
-<body>
-<div class="App" style="height:500px">
-	<?php echo "<p class='align-content-center'>" . $response . "</p>"  ?>
-</div>
-</body>
+<html>
+	<head>
+		<link href="./static/css/additional.css" rel="stylesheet">
+		<link href="./static/css/main.css" rel="stylesheet">
+		<link rel="stylesheet" href="./static/css/basic.css?v=0.0.1">
+	</head>
+	<body>
+	<div class="App" style="height:100%; padding: 100px;  display: flex; align-items: center;">
+		<?php echo "<p class='align-content-center; margin-top: -100px;'>" . $response . "</p>"  ?>
+	</div>
+	</body>
 
 </html>
